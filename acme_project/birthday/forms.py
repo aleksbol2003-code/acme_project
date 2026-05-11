3b60cee64e3f1c581bb5583548481287dab23a35
Aleks Bolotin <aleksbol2003@rambler.ru>
2026-05-11 21:50:34 +0900

    pagnator

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,19 +1,43 @@
 from django import forms  # type: ignore
+from django.core.exceptions import ValidationError  # type: ignore
 
 # Импортируем класс модели Birthday.
 from .models import Birthday
+# Импортируем функцию-валидатор.
+from .validators import real_age
 
+# Множество с именами участников Ливерпульской четвёрки.
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 # Для использования формы с моделями меняем класс на forms.ModelForm.
 class BirthdayForm(forms.ModelForm):
-    # Удаляем все описания полей.
+    # Применяем валидатор к полю birthday
+    birthday = forms.DateField(
+        validators=[real_age],
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
 
-    # Все настройки задаём в подклассе Meta.
     class Meta:
-        # Указываем модель, на основе которой должна строиться форма.
+        # Указываем модель, на основе которой должна строиться форма
         model = Birthday
-        # Указываем, что надо отобразить все поля.
-        fields = '__all__' 
-        widgets = {
-            'birthday': forms.DateInput(attrs={'type': 'date'})
-        } 
+        # Указываем, что надо отобразить все поля
+        fields = '__all__'
+
+        def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+            first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам 
+        # и возвращаем только первое имя.
+            return first_name.split()[0] 
+        
+        def clean(self):
+            # Вызов родительского метода clean.
+            super().clean()
+            # Получаем имя и фамилию из очищенных полей формы.
+            first_name = self.cleaned_data['first_name']
+            last_name = self.cleaned_data['last_name']
+            # Проверяем вхождение сочетания имени и фамилии во множество имён.
+            if f'{first_name} {last_name}' in BEATLES:
+                raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+                )
